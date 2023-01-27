0a3567996272fd46dd090f9f76761d3d59b0df07
tomasyovino <tomasyovino@gmail.com>
2023-01-27 17:31:14 -0300

    Add openai & cloudinary environment variables

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,8 +1,17 @@
 import * as dotenv from "dotenv";
+import { v2 as cloudinary } from 'cloudinary';
 
 dotenv.config();
 
 export const config = {
     PORT: process.env.PORT,
-    mongo_uri: process.env.MONGO_URI
+    mongo_uri: process.env.MONGO_URI,
+    cloudinary: {
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET
+    },
+    openAI: {
+        api_key: process.env.OPENAI_API_KEY
+    }
 };
